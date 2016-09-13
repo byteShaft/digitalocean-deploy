e5b300925dd09b426e5246f983db96cb333cae51
Omer Akram <om26er@gmail.com>
2016-09-13 17:58:25 +0500

    Remove all the shit

--- wrappers/__init__.py
+++
@@ -1,5 +0,0 @@
-from wrappers.apt import APT
-from wrappers.git import GIT
-from wrappers.lxd import LXD
-
-__all__ = ['APT', 'GIT', 'LXD']
--- wrappers/git.py
+++
@@ -1,7 +0,0 @@
-from git.repo.base import Repo
-
-
-class GIT:
-    @classmethod
-    def clone(cls, url, path):
-        Repo.clone_from(url, path)
